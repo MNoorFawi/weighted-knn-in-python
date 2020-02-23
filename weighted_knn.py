b83eace9f63f2f206cff23cdeeafd1c328bc2c91
Muhammad N. Fawi <m.noor.fawi@gmail.com>
2020-02-23 14:01:55 +0400

    Update weighted_knn.py

--- weighted_knn.py
+++ weighted_knn.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import KDTree
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 import seaborn as sns
 import random
